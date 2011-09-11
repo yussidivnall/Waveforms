2af1d616a0c6f5ede91767743188a355b8e02e7b
uri <UC.dev.null@gmail.com>
2011-09-11 07:30:15 +0100

    whateva..

--- raise_to_square.sh
+++ raise_to_square.sh
@@ -11,20 +11,27 @@ fund=110
 #Sox command: 
 #play [null input device -n] [channels -c1] synth [num of seconds] [amplitude] sin <frequency> 
 command="play -n -c1 synth 1 "
-gplotting="plot ";
+gplotting="";
 oplotting="";
-
+sum_formula=""
 idx=1
-while true; do
- frac=`echo "1/$idx"|bc -l`;
- note=`echo "($fund*$idx)"|bc -l`;
 
+while true; do
+ frac=`echo "1/$idx"|bc -l`;	#=1/n
+ note=`echo "($fund*$idx)"|bc -l`;	#=f*n
+ sum_formula="$sum_formula ($frac)*sin(x*$idx)":
  gplotting="$gplotting ($frac)*sin(x*$idx)"
  command="$command $frac sin $note "
  idx=`echo "$idx +2"|bc -l`
- echo $gplotting | gnuplot -p
+ echo "Sum Formula: $sum_formula"
 
-`$command > /dev/null 2>&1`
+ #Plotting options... 
+ #echo "plot $gplotting"
+ #echo "plot $sum_formula; pause -1"|gnuplot -p
+ echo "plot $gplotting,$sum_formula; " | gnuplot -p
+ 
+#`$command > /dev/null 2>&1`
+ sum_formula="$sum_formula +"
  gplotting="$gplotting, "
 
 done
