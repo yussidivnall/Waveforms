bdb0c38ab8fd855494bf3a0414ea3d847a447913
uri <UC.dev.null@gmail.com>
2011-09-11 03:56:35 +0100

    More work on square waves

--- raise_to_square.sh
+++ raise_to_square.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+#Build up of a square wave sound
+#Sum of odd sinosodials adding one after another and tend to a square wave
+#IE:
+#Sum 1/n sin(nx) [n=1,3,5,7...]
+#
+
+#Fundamental frequency (hz)
 fund=110
 
-
+#Sox command: 
+#play [null input device -n] [channels -c1] synth [num of seconds] [amplitude] sin <frequency> 
 command="play -n -c1 synth 1 "
+gplotting="plot ";
+oplotting="";
 
 idx=1
 while true; do
  frac=`echo "1/$idx"|bc -l`;
  note=`echo "($fund*$idx)"|bc -l`;
- echo "fraction: $frac note: $note"
+
+ gplotting="$gplotting ($frac)*sin(x*$idx)"
  command="$command $frac sin $note "
- echo $command
  idx=`echo "$idx +2"|bc -l`
- `$command`
- sleep 1
+ echo $gplotting | gnuplot -p
+
+`$command > /dev/null 2>&1`
+ gplotting="$gplotting, "
+
 done
